68c163bdcf8383bd323f92d92ca8ece314801351
artyom <artem.smolet@gmail.com>
2023-09-24 18:54:01 +0200

    First commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Copyright (c) 2009-2023 Highsoft AS
+
+import PackageDescription
+
+let package = Package(name: "test_spm",
+                      platforms: [.iOS(.v11)],
+                      products: [
+                        .library(name: "test_spm",
+                                targets: ["Highcharts"])
+                      ],
+                      targets: [.binaryTarget(name: "test_spm",
+                                              path: "test_spm.xcframework")]
+                    )
+)
