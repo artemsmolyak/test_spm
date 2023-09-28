2e6ee654fff3bd29055d45f2decfdf2170dbeda7
artyom <artem.smolet@gmail.com>
2023-09-28 20:33:39 +0200

    Add more frameworks

--- Package.swift
+++ Package.swift
@@ -4,9 +4,21 @@
 
 import PackageDescription
 
-let package = Package(name: "test_framework",
-                      platforms: [.iOS(.v11)],
-                      products: [.library(name: "test_framework",
-                                          targets: ["test_framework"])],
-                      targets: [.binaryTarget(name: "test_framework",
-                                              path: "test_framework.xcframework")])
+let package = Package(
+    name: "test_framework",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "test_framework",
+            targets: ["test_framework"]),
+        .library(
+            name: "test_framework2",
+            targets: ["test_framework2"]),
+    ],
+    targets: [
+        .binaryTarget(name: "test_framework",
+                      path: "test_framework.xcframework"),
+        .binaryTarget(name: "test_framework2",
+                      path: "test_framework2.xcframework"),
+    ]
+)
